42d5368d3b8193bf9b05888460b0928a0677cfa1
Ukeje Michgolden <michgoldennathan17@gmail.com>
2020-02-24 02:57:43 +0100

    [Feature] Add eslint for Js linting

--- src/script.js
+++ src/script.js
@@ -1,25 +1,25 @@
 const selectQuery = query => document.querySelector(query);
 
-const banner = selectQuery(".banner");
-const output = selectQuery(".inner");
-const result = selectQuery(".result");
-const alert = selectQuery(".alert");
-const powerON = selectQuery("#ON");
-const clear = selectQuery(".clear");
+const banner = selectQuery('.banner');
+const output = selectQuery('.inner');
+const result = selectQuery('.result');
+const alert = selectQuery('.alert');
+const powerON = selectQuery('#ON');
+const clear = selectQuery('.clear');
 powerON.onclick = switchOn;
 
-let feedInput = e => {
+const feedInput = e => {
   const keyboardInput = document.querySelector(`button[data-key="${e.keyCode}"]`);
-  const controls = ["Enter", "ON", "AC", "OFF"];
+  const controls = ['Enter', 'ON', 'AC', 'OFF'];
   if (!keyboardInput) return;
   !controls.includes(keyboardInput.innerText)
     ? wrapOpKeys(keyboardInput.innerText)
     : wrapCtrlKeys(keyboardInput.innerText);
 };
-window.addEventListener("keydown", feedInput);
+window.addEventListener('keydown', feedInput);
 
 function wrapOpKeys(text) {
-  let regexNumbers = /[^0-9]/;
+  const regexNumbers = /[^0-9]/;
   if (regexNumbers.test(text)) {
     getOperators(text);
   } else {
@@ -30,38 +30,38 @@ function wrapOpKeys(text) {
 
 function wrapCtrlKeys(text) {
   switch (text) {
-    case "Enter":
+    case 'Enter':
       calculator();
       break;
-    case "AC":
+    case 'AC':
       deleteItems();
       break;
-    case "OFF":
+    case 'OFF':
       location.reload();
       break;
-    case "ON":
+    case 'ON':
       switchOn();
       break;
   }
 }
 
-const allScreenButtons = Array.from(document.querySelectorAll("button"));
+const allScreenButtons = Array.from(document.querySelectorAll('button'));
 
 function switchOn() {
-  alert.textContent = "Calculator Powered";
+  alert.textContent = 'Calculator Powered';
   allScreenButtons.forEach(button => {
-    button.style.color = "green";
+    button.style.color = 'green';
   });
-  output.style.backgroundColor = "gray";
-  result.style.backgroundColor = "gray";
-  setTimeout(() => (alert.style.display = "none"), 1200);
-  powerON.style.display = "none";
-  clear.style.display = "inline";
+  output.style.backgroundColor = 'gray';
+  result.style.backgroundColor = 'gray';
+  setTimeout(() => (alert.style.display = 'none'), 1200);
+  powerON.style.display = 'none';
+  clear.style.display = 'inline';
   buttonEvents();
 }
 
-let objCollector = {
-  inputs: []
+const objCollector = {
+  inputs: [],
 };
 
 function pickOperator(x) {
@@ -71,19 +71,19 @@ function pickOperator(x) {
 function buttonEvents() {
   allScreenButtons.forEach(button => {
     const buttonText = button.id;
-    let regexButtons = /\d/;
+    const regexButtons = /\d/;
 
-    button.addEventListener("click", () => {
+    button.addEventListener('click', () => {
       if (regexButtons.test(+buttonText)) {
         pickOperator(buttonText);
         updateScreen();
-      } else if (buttonText === "Enter") {
+      } else if (buttonText === 'Enter') {
         calculator();
-      } else if (buttonText === "AC") {
+      } else if (buttonText === 'AC') {
         deleteItems();
-      } else if (buttonText === "ON") {
+      } else if (buttonText === 'ON') {
         switchOn;
-      } else if (buttonText === "OFF") {
+      } else if (buttonText === 'OFF') {
         location.reload();
       } else {
         getOperators(buttonText);
@@ -93,10 +93,10 @@ function buttonEvents() {
 }
 
 function getOperators(buttonText) {
-  let regex = /x|\+|\.|\-|\//;
+  const regex = /x|\+|\.|\-|\//;
   if (
-    objCollector.inputs.length >= 1 &&
-    !regex.test(objCollector.inputs[objCollector.inputs.length - 1])
+    objCollector.inputs.length >= 1
+    && !regex.test(objCollector.inputs[objCollector.inputs.length - 1])
   ) {
     pickOperator(buttonText);
     updateScreen();
@@ -109,21 +109,21 @@ function deleteItems() {
 }
 
 function calculator() {
-  let joinedInputs = objCollector.inputs.join("");
+  let joinedInputs = objCollector.inputs.join('');
   joinedInputs = joinedInputs.replace(/x/g, '*').replace(/÷/g, '/');
-  let result = eval(joinedInputs);
+  const result = eval(joinedInputs);
   updateCollector(result);
 }
 
 function updateScreen() {
-  let print = objCollector.inputs.join("");
+  const print = objCollector.inputs.join('');
   output.textContent = print;
-  result.textContent = "";
+  result.textContent = '';
 }
 
 function updateCollector(arg) {
   if (arg === Infinity || typeof arg === NaN) {
-    result.textContent = "Error";
+    result.textContent = 'Error';
     objCollector.inputs.splice(0);
   } else {
     result.textContent = arg;
